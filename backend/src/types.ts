65dcb0bafe5c6138c81f3a7cc5a1e43435252799
mariotawfelis <mario.tawfelis@outlook.com>
2024-12-17 20:43:00 +0100

    Added enum for target audiences

---
+++ backend/src/types.ts
@@ -0,0 +1,7 @@
+export enum TargetAudiences {
+    ScientistsResearchers = "Scientists and Researchers",
+    StudentsAcademics = "Students and Academics",
+    IndustryProfessionals = "Industry Professionals",
+    JournalistsMedia = "Journalists and Media Professionals",
+    GeneralPublic = "General Public",
+}
